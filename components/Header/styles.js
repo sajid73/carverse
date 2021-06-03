3c8d1f571077633a0ac876a98f30062f6092133a
sajid73 <smsajid7300@gmail.com>
2021-06-03 21:20:37 +0600

    design changed

--- components/Header/styles.js
+++ components/Header/styles.js
@@ -1,24 +1,26 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-        position: "absolute",
-        top: 50,
-        zIndex: 100,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '100%',
-        paddingHorizontal: 20,
-    },
-    logo: {
-        width: 100,
-        height: 20,
-        resizeMode: 'contain',
-    },
-    menu: {
-        width: 25,
-        height: 25,
-        resizeMode: 'contain',
-    }
+  container: {
+    position: 'absolute',
+    top: 50,
+    zIndex: 100,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    top: -70,
+    resizeMode: 'contain',
+  },
+  menu: {
+    width: 25,
+    height: 25,
+    resizeMode: 'contain',
+  }
 });
+
 export default styles;
