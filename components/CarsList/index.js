841d74494324e8ee85ff2d2eb4dfb59eeae87416
Ariful Islalm <arifsujon.bd@gmail.com>
2021-06-03 19:30:07 +0600

    add flat list snap

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Dimensions } from 'react-native';
 import styles from './styles';
 import cars from './cars';
 import CarItem from '../CarItem';
@@ -11,6 +11,10 @@ const CarsList = (props) => {
             <FlatList 
                 data={cars} 
                 renderItem ={({item}) => <CarItem car={item}/>}
+                // showsVerticalScrollIndicator = {false}
+                snapToAlignment = {'start'}
+                decelerationRate = {'fast'}
+                snapToInterval = {Dimensions.get('window').height}
             />
         </View>
     )
